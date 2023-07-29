066a832e7057eb5b5171b05010d688d6875b6b3b
terkrub <terkrub9@gmail.com>
2023-07-29 16:36:02 +1200

    first

---
+++ cise_ass1a_worksheet2/cise_ass1a_worksheet2/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 5000);
+}
+bootstrap();
